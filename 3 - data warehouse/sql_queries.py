f9a223f707ab8f772fef934da6687adbefd50a48
Sergey Bondarev <bsa0393@gmail.com>
2021-12-27 15:40:22 +0300

    make creation queries work

--- 3 - data warehouse/sql_queries.py
+++ 3 - data warehouse/sql_queries.py
@@ -9,7 +9,7 @@ config.read('dwh.cfg')
 STAGING_EVENTS = "staging_events"
 STAGING_SONGS = "staging_songs"
 SONGPLAY = "songplay"
-USER = "user"
+USER = "users"
 SONG = "song"
 ARTIST = "artist"
 TIME = "time"
@@ -26,7 +26,7 @@ time_table_drop = f"DROP TABLE IF EXISTS {TIME}"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create= (f"""
     CREATE TABLE IF NOT EXISTS {STAGING_EVENTS} (
         artist VARCHAR(100),
         auth VARCHAR(20),
@@ -49,7 +49,7 @@ staging_events_table_create= ("""
     );
 """)
 
-staging_songs_table_create = ("""
+staging_songs_table_create = (f'''
     CREATE TABLE IF NOT EXISTS {STAGING_SONGS} (
         num_songs INTEGER,
         artist_id VARCHAR(20),
@@ -62,29 +62,29 @@ staging_songs_table_create = ("""
         duration FLOAT,
         year INTEGER
     );
-""")
+''')
 
-songplay_table_create = ('''
+songplay_table_create = (f'''
     CREATE TABLE IF NOT EXISTS {SONGPLAY} (
-        songplay_id int IDENTITY(0,1) PRIMARY KEY,
+        songplay_id INTEGER IDENTITY(0,1) PRIMARY KEY,
         start_time timestamp NOT NULL,
-        user_id int INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
         level VARCHAR(20),
         song_id VARCHAR(20),
         artist_id VARCHAR(20),
-        session_id INT,
+        session_id INTEGER,
         location VARCHAR(50),
         user_agent VARCHAR(200),
-        FOREIGN KEY (song_id) REFERENCES song (song_id),
-        FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
-        FOREIGN KEY (user_id) REFERENCES user (user_id),
-        FOREIGN KEY (start_time) REFERENCES time (start_time)
+        FOREIGN KEY (song_id) REFERENCES {SONG} (song_id),
+        FOREIGN KEY (artist_id) REFERENCES {ARTIST} (artist_id),
+        FOREIGN KEY (user_id) REFERENCES {USER} (user_id),
+        FOREIGN KEY (start_time) REFERENCES {TIME} (start_time)
     );
 ''')
 
 user_table_create = (f'''
     CREATE TABLE IF NOT EXISTS {USER} (
-        user_id int PRIMARY KEY,
+        user_id INTEGER PRIMARY KEY,
         first_name VARCHAR(50),
         last_name VARCHAR(50),
         gender VARCHAR(20),
@@ -97,12 +97,12 @@ song_table_create = (f'''
         song_id VARCHAR(20) PRIMARY KEY,
         title VARCHAR(200),
         artist_id VARCHAR(20),
-        year INT,
+        year INTEGER,
         duration FLOAT
     );
 ''')
 
-artist_table_create = ('''
+artist_table_create = (f'''
     CREATE TABLE IF NOT EXISTS {ARTIST} (
         artist_id VARCHAR(20) PRIMARY KEY,
         name VARCHAR(200),
@@ -126,11 +126,21 @@ time_table_create = (f'''
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = (f"""
+    COPY {STAGING_EVENTS}
+    from '{config['S3']['LOG_DATA']}'
+    iam_role '{config['IAM_ROLE']['ARN']}'
+    region 'us-west-2'
+    json 'auto'
+""")
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = (f"""
+    COPY {STAGING_SONGS}
+    from '{config['S3']['SONG_DATA']}'
+    iam_role '{config['IAM_ROLE']['ARN']}'
+    region 'us-west-2'
+    json 'auto'
+""")
 
 # FINAL TABLES
 
@@ -151,7 +161,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
