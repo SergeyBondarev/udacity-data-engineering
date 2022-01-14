10760dc700a1228fe171a8d3c67eee328fb051cd
Sergey Bondarev <bsa0393@gmail.com>
2022-01-14 17:25:59 +0000

    establish etl pipeline for songs and artists

--- 4 - data lake/etl.py
+++ 4 - data lake/etl.py
@@ -2,13 +2,19 @@ import configparser
 from datetime import datetime
 import os
 import json
+from re import MULTILINE
+from logger import setup_logger
 
 from pyspark.sql import SparkSession
 
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
-from pyspark.sql.types import StructType
+from pyspark.sql.types import StructType, StructField, StringType, FloatType, IntegerType, DateType
+
+
+import logging
+logger = logging.getLogger('etl')
 
 
 SONG_SCHEMA_PATH = "schema/song_data.json"
@@ -35,32 +41,51 @@ def create_spark_session():
     return spark
 
 
-def get_schema(config_file_path):
+def get_schema(config_file_path=None):
+    if config_file_path is None:
+        return StructType(
+            [
+                StructField("num_songs", IntegerType(), True),
+                StructField("artist_id", StringType(), True),
+                StructField("artist_latitude", FloatType(), True),
+                StructField("artist_longitude", FloatType(), True),
+                StructField("artist_location", StringType(), True),
+                StructField("artist_name", StringType(), True),
+                StructField("song_id", StringType(), True),
+                StructField("title", StringType(), True),
+                StructField("duration", FloatType(), True),
+                StructField("year", IntegerType(), True)
+            ])
+
+    return get_schema_from_config_file(config_file_path)
+
+def get_schema_from_config_file(config_file_path):
     with open(config_file_path, 'r', encoding='utf-8') as f:
         schema = json.load(f)
         return StructType.fromJson(schema)
 
 
 
+
 def process_song_data(spark, input_data, output_data):
-    song_data = f"{input_data}/{SONG_DATA}"
-    song_data_schema = get_schema(SONG_SCHEMA_PATH)
+    song_data = f"{input_data}/{SONG_DATA}/*/*/*/*.json"
+    song_data_schema = get_schema()
 
-    print('spark is reading json from s3...')
-    df = spark.read.json(song_data, schema=song_data_schema)
+    logger.info('spark is reading json files from s3...')
 
+    df = spark.read.option("recursiveFileLookup", "true").json(song_data, multiLine=True, schema=song_data_schema)
+
+    logger.info('size of RDD is %d' % df.count())
     
     songs_table = df.select("song_id", "title", "artist_id", "year", "duration").dropDuplicates()
-    songs_table.write.parquet(f"{output_data}/songs_table", mode="overwrite")
-    
-    # write songs table to parquet files partitioned by year and artist
-    # songs_table
 
-    # extract columns to create artists table
-    # artists_table = 
+    logger.info('size of RDD is %d' % songs_table.count())
+
+    songs_table.write.partitionBy("year", "artist_id").parquet(f"{output_data}/songs", mode="overwrite")
+
+    artists_table = df.select("artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude").dropDuplicates() 
     
-    # write artists table to parquet files
-    # artists_table
+    artists_table.write.partitionBy("artist_id").parquet(f"{output_data}/artists", mode="overwrite")
 
 
 def process_log_data(spark, input_data, output_data):
@@ -105,6 +130,7 @@ def process_log_data(spark, input_data, output_data):
 
 
 def main():
+    setup_logger()
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = "s3://sparkify-parquet/"
